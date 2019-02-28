1adbd5adb42ebfcb01a7b4fb62bb9b52346bdf28
Hacart Anthony <anthony_hacart@ens.univ-artois.fr>
2019-02-28 08:53:21 +0100

    update

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Game;
+use App\Entity\Team;
 use App\Form\GameType;
 use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,12 +27,12 @@ class GameController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="game_new", methods={"GET","POST"})
+     * @Route("/new/{id}", name="game_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, Team $team = null): Response
     {
         $game = new Game();
-        $form = $this->createForm(GameType::class, $game);
+        $form = $this->createForm(GameType::class, $game, ['teamA' => $team]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -3,28 +3,73 @@
 namespace App\Form;
 
 use App\Entity\Game;
+use App\Entity\Team;
+use App\Form\Type\ScoreType;
+use App\Form\Type\TeamType;
+use App\Form\Type\VersusType;
+use App\Repository\TeamRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GameType extends AbstractType
 {
+    private $teamA;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->teamA = $options['teamA'];
         $builder
-            ->add('scoreTeamA')
-            ->add('scoreTeamB')
-            ->add('date')
+            ->add('scoreTeamA', ScoreType::class)
+            ->add('scoreTeamB', ScoreType::class)
+            ->add('date', DateType::class, [
+                'widget' => 'single_text'
+            ])
             ->add('rating')
             ->add('teamA')
             ->add('teamB')
         ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            array($this, 'preSetData')
+        );
+    }
+
+    public function preSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        $match = $event->getData();
+
+        if ($this->teamA !== null && $this->teamA instanceof Team) {
+            $match->setTeamA($this->teamA);
+
+            $form->remove('teamA')
+                ->remove('teamB')
+                ->remove('date')
+                ->remove('scoreTeamA')
+                ->remove('rating')
+                ->remove('scoreTeamB');
+
+            $form->add('teamB', TeamType::class, [
+                'query_builder' => function (TeamRepository $tr) {
+                    return $tr->createQueryBuilder('tr')
+                        ->andWhere('tr != :val')
+                        ->setParameter(':val', $this->teamA);
+                },
+            ]);
+        }
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Game::class,
+            'teamA' => null
         ]);
     }
 }
